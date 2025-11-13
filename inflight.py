58babc91aff1702586fa1da0c532979d3b5b8015
Arvindh Manian <arvindh.manian@gmail.com>
2025-11-13 14:46:07 -0500

    minor testing

---
+++ inflight.py
@@ -0,0 +1,62 @@
+from sortedcontainers import SortedList
+from typing import Optional
+# once we have RTT estimates
+# we can form a congestion window
+
+class InflightTracker:
+    def __init__(self):
+        self.inflight_state_numbers: SortedList[int] = SortedList()
+        self.dependencies: dict[int, Optional[int]] = {}
+        self.inflight_dependencies: SortedList[int] = SortedList()
+        self.highest_ack = -1
+        # TODO: discard old states and dependencies
+        # not important for correctness, just efficiency
+
+    def acked(self, state_number: int) -> None:
+        all_acked = []
+        ind = self.inflight_state_numbers.bisect_left(state_number)
+        while ind >= 0:
+            all_acked.append(self.inflight_state_numbers[0])
+            self.inflight_state_numbers.pop(index=0)
+            ind -= 1
+        
+        assert len(all_acked) >= 1
+
+        for acked_state_number in all_acked:
+            match self.dependencies[acked_state_number]:
+                case None:
+                    pass
+                case x:
+                    self.inflight_dependencies.remove(self.dependencies[acked_state_number])
+
+        self.highest_ack = state_number
+
+    def sent(self, state_number: int, depends_on: Optional[int]):
+        if depends_on is not None:
+            self.inflight_dependencies.add(depends_on)
+
+        self.dependencies[state_number] = depends_on
+        self.inflight_state_numbers.add(state_number)
+
+    def min_inflight_dependency(self) -> Optional[int]:
+        return self.inflight_dependencies[0] if len(self.inflight_dependencies) > 0 else None 
+
+if __name__ == "__main__":
+    # test 1
+    it: InflightTracker = InflightTracker()
+    it.sent(0, None)
+    it.sent(1, 0)
+    assert it.min_inflight_dependency() == 0
+
+    it.acked(0)
+    assert it.min_inflight_dependency() == 0
+
+    it.sent(2, 1)
+    it.sent(3, 1)
+    assert it.min_inflight_dependency() == 0
+    it.acked(2)
+
+    assert it.min_inflight_dependency() == 1, print(f'{it.min_inflight_dependency()}, expected 1')
+    it.acked(3)
+    assert it.min_inflight_dependency() is None
+
--- sender.py
+++ sender.py
@@ -1,38 +1,4 @@
-from sortedcontainers import SortedList
-from transport import TransportInstruction
-# once we have RTT estimates
-# we can form a congestion window
-
-class InflightTracker:
-    def __init__(self):
-        self.inflight_state_numbers: SortedList[int] = SortedList()
-        self.dependencies: dict[int, int] = {}
-        self.inflight_dependencies: SortedList[int] = SortedList()
-        self.highest_ack = -1
-        # TODO: discard old states and dependencies
-        # not important for correctness, just efficiency
-
-    def acked(self, state_number: int) -> None:
-        all_acked = []
-        ind = self.inflight_state_numbers.bisect_left(state_number)
-        while ind >= 0:
-            all_acked.append(self.inflight_state_numbers[0])
-            ind -= 1
-        
-        assert len(all_acked) >= 1
-
-        for acked_state_number in all_acked:
-            self.inflight_dependencies.remove(self.dependencies[acked_state_number])
-
-        self.highest_ack = state_number
-
-    def sent(self, state_number: int, depends_on: int):
-        self.dependencies[state_number] = depends_on
-        self.inflight_state_numbers.add(state_number)
-        self.inflight_dependencies.add(depends_on)
-
-    def min_inflight_dependency(self):
-        return self.inflight_dependencies[0] if len(self.inflight_dependencies) > 0 else 2 ** 31 - 1
+from inflight import InflightTracker
 
 def on_ack(state_number: int, inf: InflightTracker) -> None:
     # this ack acknowledges all inflights that were <= state_number
@@ -44,9 +10,8 @@ def on_send(new_state: 'State', inf: InflightTracker) -> None:
     new_num = new_state.index
     ack_num = highest_ack
     throwaway_num = min(inf.min_inflight_dependency(), highest_ack) - 1
-    t: TransportInstruction = TransportInstruction(old_num, new_num, ack_num, throwaway_num, new_state.diff(states[highest_ack]))
+    # TODO: sending logic
     inf.sent(new_num, old_num)
-    carrier.send(t)
 
 
 def main():
