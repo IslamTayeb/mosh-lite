6fb9e62684f74614b0cc71f995c517f3b3da8498
Arvindh Manian <arvindh.manian@gmail.com>
2025-11-17 18:40:24 -0500

    move functions inside class

--- datagram.py
+++ datagram.py
@@ -11,44 +11,45 @@ class Packet:
     ts_reply: int
     payload: bytes
 
-def pack(p: Packet) -> bytes:
+    def pack(self) -> bytes:
 
-    direction = p.direction
-    seq = p.seq
-    ts = p.ts
-    ts_reply = p.ts_reply
-    payload = p.payload
-    
-    assert 0 <= seq < (1 << 63)
+        direction = self.direction
+        seq = self.seq
+        ts = self.ts
+        ts_reply = self.ts_reply
+        payload = self.payload
+        
+        assert 0 <= seq < (1 << 63)
 
-    dir_bit = int(direction)
-    nonce = (dir_bit << 63) | seq
+        dir_bit = int(direction)
+        nonce = (dir_bit << 63) | seq
 
-    ts = ts & 0xFFFF
-    assert 0 <= ts_reply <= 0xFFFF
-    
-    header = struct.pack(DATAGRAM_FORMAT_STRING, nonce, ts, ts_reply)
+        ts = ts & 0xFFFF
+        assert 0 <= ts_reply <= 0xFFFF
+        
+        header = struct.pack(DATAGRAM_FORMAT_STRING, nonce, ts, ts_reply)
 
-    # TODO: What do we do if this is larger than the MTU of the network?
-    return header + payload
+        # TODO: What do we do if this is larger than the MTU of the network?
+        return header + payload
 
-def unpack(value: bytes) -> Packet:
-    header_length = struct.calcsize(DATAGRAM_FORMAT_STRING) 
+    @staticmethod
+    def unpack(value: bytes) -> 'Packet':
+        header_length = struct.calcsize(DATAGRAM_FORMAT_STRING) 
 
-    nonce, ts, ts_reply = struct.unpack_from(DATAGRAM_FORMAT_STRING, value, offset=0)
-    payload = value[header_length:]
+        nonce, ts, ts_reply = struct.unpack_from(DATAGRAM_FORMAT_STRING, value, offset=0)
+        payload = value[header_length:]
 
-    return Packet(
-                bool(nonce & (1 << 63)),
-                nonce & ((1 << 63) - 1),
-                ts,
-                ts_reply, 
-                payload
-          )
+        return Packet(
+                    bool(nonce & (1 << 63)),
+                    nonce & ((1 << 63) - 1),
+                    ts,
+                    ts_reply, 
+                    payload
+              )
 
 if __name__ == "__main__":
     original_packet = Packet(True, 7, 10, 5, 'abc'.encode('utf-8'))
-    back = unpack(pack(original_packet))
+    back = Packet.unpack(original_packet.pack())
     assert original_packet.direction == back.direction
     assert original_packet.seq == back.seq
     assert original_packet.ts == back.ts
