68db584ed65a87154b8d418c483963870b507d3b
Islam M. Tayeb <islam.tayeb@duke.edu>
2025-11-18 18:51:30 -0500

    implement sender.py for helpers

--- mosh/sender.py
+++ mosh/sender.py
@@ -1,22 +1,43 @@
 from inflight import InflightTracker
+from state import State
+from transport import Transporter, TransportInstruction
+import socket
 
-def on_ack(state_number: int, inf: InflightTracker) -> None:
-    # this ack acknowledges all inflights that were <= state_number
-    inf.acked(state_number)
-    
+current_state = State("")
+inflight = InflightTracker()
+receiver_ack = -1
+transport = None
+# SHOULDN'T BE NECESSARY ONCE WE HAVE A RECEIVER ACK
+last_state_num = 0
 
-def on_send(new_state: 'State', inf: InflightTracker) -> None:
-    old_num = inf.highest_ack 
-    new_num = new_state.index
-    ack_num = highest_ack
-    throwaway_num = min(inf.min_inflight_dependency(), highest_ack) - 1
-    # TODO: sending logic
-    inf.sent(new_num, old_num)
+def on_receive(instruction: TransportInstruction):
+    global receiver_ack
+    if instruction.ack_num > receiver_ack:
+        inflight.acked(instruction.ack_num)
+        receiver_ack = instruction.ack_num
 
+def on_send(new_state: State, inf: InflightTracker):
+    global current_state, transport, last_state_num
+    diff = current_state.generate_patch(new_state)
+    old_num = last_state_num
+    new_num = old_num + 1
+    last_state_num = new_num
 
-def main():
-    InflightTracker inf = InflightTracker()
-    # need to register handlers for keystrokes and responses
+    min_dep = inf.min_inflight_dependency()
+    throwaway_num = (min_dep - 1) if min_dep else -1
 
-if __name__ == "__main__":
-    main()
+    transport.send(old_num, new_num, receiver_ack, throwaway_num, diff)
+    inf.sent(new_num, old_num - 1 if old_num > 0 else None)
+    current_state = new_state
+
+def init(host, port):
+    global transport
+    transport = Transporter(host, port, on_receive)
+    transport.socket.settimeout(0.1)
+
+def receive_acks():
+    while True:
+        try:
+            transport.recv()
+        except socket.timeout:
+            break
--- mosh/test_sender.py
+++ mosh/test_sender.py
@@ -1,15 +1,10 @@
-from transport import Transporter
+from sender import init, on_send, receive_acks, inflight
 from state import State
 
-t = Transporter('127.0.0.1', 53001, lambda x: None)
+init('127.0.0.1', 53001)
 
-str1 = State('abc')
-str2 = State('bcdef')
-str3 = State('abc')
+on_send(State("abc"), inflight)
+on_send(State("bcdef"), inflight)
+on_send(State("abc"), inflight)
 
-delta1 = State("").generate_patch(str1)
-t.send(0, 1, 0, 0, delta1)
-delta2 = str1.generate_patch(str2)
-t.send(1, 2, 0, 0, delta2)
-delta3 = str2.generate_patch(str3)
-t.send(2, 3, 0, 0, delta3)
+receive_acks()
